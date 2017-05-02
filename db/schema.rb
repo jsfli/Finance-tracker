5399f291eda1f15c81572b893209465c67cdb00f
Jeffrey Li <jeffrey.li@cantab.net>
2017-05-02 15:55:35 +0800

    added user and stocks association and display

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170501095149) do
+ActiveRecord::Schema.define(version: 20170502024952) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,13 @@ ActiveRecord::Schema.define(version: 20170501095149) do
     t.string   "ticker"
     t.string   "name"
     t.decimal  "last_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_stocks", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "stock_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
